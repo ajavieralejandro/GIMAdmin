443091bb65e39d52b1ce9fb3c378dd6d861f8894
ajavieralejandro <amorosijavier@gmail.com>
2024-02-13 12:28:03 -0300

    nav dropdown functionality

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,4 +1,14 @@
+
+import NavbarDropdown from "../NavbarDropdown/navbar.dropdown";
+import { useState } from "react";
+
+
 const Navbar = () =>{
+  const [navMenu,setNavMenu] = useState(false);
+  const [navMenu1,setNavMenu1] = useState(false);
+  const [navMenu2,setNavMenu2] = useState(false);
+
+
     return (
         <>
 <nav className="flex justify-between px-20 py-10 items-center bg-blue-200">
@@ -6,9 +16,10 @@ const Navbar = () =>{
   <div className="flex items-center">
     
     <ul className="flex items-center space-x-3 md:space-x-6">
-      <li className="font-semibold text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <li onClick={()=>setNavMenu(!navMenu)} className="font-semibold text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
+      {navMenu?<NavbarDropdown />:null}
 </li>
       <li className="font-semibold text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
@@ -26,16 +37,18 @@ const Navbar = () =>{
 </svg>
 
       </li>
-      <li>
+      <li onClick={()=>setNavMenu1(!navMenu1)}>
 	  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
 </svg>
+{navMenu1?<NavbarDropdown />:null}
 
       </li>
-	  <li>
+	  <li onClick={()=>setNavMenu2(!navMenu2)}>
 	  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
 </svg>
+    {navMenu2?<NavbarDropdown />:null}
 
 	  </li>
 	  <li>
