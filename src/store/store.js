f98a41d1baf86071ea515bb3c5e224abc1ee3c5a
ajavieralejandro <amorosijavier@gmail.com>
2024-03-22 20:31:32 -0300

    changes movimientos

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,18 @@
 import {compose,createStore,applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
-
+import { persistStore,persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 const middlewares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
 //root-reducer
+const persistConfig = {
+    key:'root',
+    storage 
+}
 
+const persistedReducer = persistReducer(persistConfig,rootReducer);
 
-const store = createStore(rootReducer,undefined,composedEnhancers);
-export default store;
+export const store = createStore(persistedReducer,undefined,composedEnhancers);
+export const persistor = persistStore(store); 
