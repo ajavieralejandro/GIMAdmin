5fdcd6b96027705ac0fca3e6330cfa26b3ac5356
ajavieralejandro <amorosijavier@gmail.com>
2024-03-31 18:30:41 -0300

    justify center navbar responsive

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -21,9 +21,9 @@ const Navbar = () =>{
 
     return (
         <>
-<nav className="flex justify-between px-20 py-10 items-center bg-blue-200">
+<nav className="flex justify-center md:justify-between md:px-20 py-10 items-center bg-slate-200">
   <h1 className="text-xl text-gray-800 hidden md:flex  font-bold">Mis Actividades</h1>
-  <div className="flex items-center">
+  <div className="">
     
     <ul className="flex items-center space-x-3 md:space-x-6">
       <li onClick={()=>setNavMenu(!navMenu)} className="font-semibold text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
