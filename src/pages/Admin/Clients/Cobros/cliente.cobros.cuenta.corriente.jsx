4efb1b47a5a16838775743c93d98eff2195c8910
ajavieralejandro <amorosijavier@gmail.com>
2024-04-17 19:03:02 -0300

    Deuda Registrada con Exito

--- src/pages/Admin/Clients/Cobros/cliente.cobros.cuenta.corriente.jsx
+++ src/pages/Admin/Clients/Cobros/cliente.cobros.cuenta.corriente.jsx
@@ -1,7 +1,11 @@
 import { useState,useEffect } from "react";
 import { Select,Button } from "antd";
+import { useSelector } from "react-redux";
 import ProductosTable from "../../../../components/ProductosTable/productos.table";
 const ClienteCobroCuentaCorriente = ({setPage})=>{
+    let user_id = useSelector(state=>state.user.currentUser.id);
+    let client_id = useSelector(state=>state.client.currentClient.id);
+    let caja_id = useSelector(state=>state.user.currentUser.caja);
     const [productos,setProductos]=useState([]);
     const [cuenta,setCuenta]=useState([]);
     useEffect(()=>{
@@ -16,6 +20,13 @@ const ClienteCobroCuentaCorriente = ({setPage})=>{
     },[]);
 
     const registrarCobro = () =>{
+        let cobro_name = "";
+        let total_price = 0;
+        cuenta.map(producto=>{
+            if(producto.cantidad>0)
+                total_price += producto.price * producto.cantidad;
+                cobro_name = "cobro : "+producto.name+" cantidad :"+producto.cantidad+"\n";
+        })
         //Chequear que exista stock para registrar cobro
         cuenta.forEach(element=>{
             let cantidad = parseInt(element.cantidad);
@@ -25,8 +36,43 @@ const ClienteCobroCuentaCorriente = ({setPage})=>{
             }
         })
         //Tengo que registrar el cobro
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body :JSON.stringify({
+              "user_id": user_id,
+              "caja_id" : caja_id,
+              "client_id" : client_id,
+              "status" : "green",
+              "vencimiento" : (new Date().toDateString()),
+               "name" : cobro_name,
+               "price" : total_price
+
+
+            })}
+            fetch("https://stingray-app-4224s.ondigitalocean.app/api/v1/deudas",requestOptions)
+            .then(res=>res.text())
+            .then((data=>alert(data)));
         
         //una vez registrado el cobro tengo que sacar los elementos del stock
+        cuenta.map(producto=>{
+            //actualizo el stock de prodcutos que se consumieron
+            if(producto.cantidad>0){
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body :JSON.stringify({
+                      "stock":producto.stock,     
+                    })}
+                fetch(`https://stingray-app-4224s.ondigitalocean.app/api/v1/producto/restar_stock/${producto.id}`,requestOptions)
+                .then(res=>res.json())
+                .then(()=>setPage(0))
+
+            }
+        })
+        //Falta cargar los puntos por referido
+
 
     }
 
