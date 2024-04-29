1b711f998b38b5cd8c5996b97ea49faaf7d3fb6e
ajavieralejandro <amorosijavier@gmail.com>
2024-04-28 22:45:54 -0300

    More Advances...

--- src/pages/Admin/Ventas/horarios.jsx
+++ src/pages/Admin/Ventas/horarios.jsx
@@ -5,6 +5,7 @@ import {useState,useEffect} from 'react'
 export default function Calendar() {
   const [events,setEvents] = useState([]);
   const [sucursales,setSucursales] = useState([]);
+ 
   useEffect(()=>{
     fetch('https://stingray-app-4224s.ondigitalocean.app/api/v1/sucursales')
     .then(res=>res.json())
@@ -16,22 +17,38 @@ export default function Calendar() {
       })
       setSucursales(aux);
     })
-    fetch('https://stingray-app-4224s.ondigitalocean.app/api/v1/clases_programadas')
+    fetch('https://stingray-app-4224s.ondigitalocean.app/api/v1/clases')
     .then(res=>res.json())
     .then(data=>{
       console.log("eventos :",data.data);
       let aux = data.data.map(element=>{
+        // Parse the date string into a Date object
+var date = new Date(element.dia);
+
+// Extract the year, month, and day components
+var year = date.getFullYear();
+var month = ('0' + (date.getMonth() + 1)).slice(-2); // Adding 1 because getMonth() returns zero-based index
+var day = ('0' + date.getDate()).slice(-2);
+
+// Form the Y-M-D format
+var formattedDate = year + '-' + month + '-' + day+'T';
+        return({...element,formattedDate:formattedDate})
+        
+      })
+      aux = aux.map(element=>{
         return({...element,title:element.name,
-                start:"2024-04-25T"+element.start,
-                end:"2024-04-25T"+element.end
+                start:element.formattedDate+element.start,
+                end:element.formattedDate+element.end
 
         })
       })
       console.log("aux es :",aux);
       setEvents(aux);
     })
-    console.log("Los eventos son :",events)
+    console.log("Los eventos son :",events);
+
   },[])
+
   return (
     <div>
     <h1 className="text-3xl text-left">Horarios de las Actividades Programadas</h1>
